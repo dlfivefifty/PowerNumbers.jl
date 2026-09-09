f5f9ce8ca445ecc337bfb7a8235f01e8bf63b80b
Sheehan Olver <solver@mac.com>
2026-09-09 14:02:27 +0100

    overloads to support SingularIntegrals.jl

--- src/LogNumber.jl
+++ src/LogNumber.jl
@@ -9,11 +9,21 @@ struct LogNumber{T} <: Number
 end
 
 LogNumber(s, t) = LogNumber(promote(s, t)...)
-LogNumber(x::Real) = LogNumber{typeof(x)}(x)
-LogNumber{T}(x::Real) where T = LogNumber{T}(zero(T), x)
+LogNumber(x::Number) = LogNumber{typeof(x)}(x)
+LogNumber{T}(x::Number) where T = LogNumber{T}(zero(T), x)
+LogNumber(x::LogNumber) = x
+LogNumber{T}(x::LogNumber) where T = LogNumber{T}(x.s, x.c)
 
-function Real(x::LogNumber)
+const RealOrComplex = Union{Real, Complex}
+
+function Real(x::LogNumber{T}) where T
     isreal(x) || throw(InexactError(:Real, x))
+    isinf(x) && convert(T, sign(x)*Inf)
+    x.c
+end
+
+function RealOrComplex(x::LogNumber{T}) where T
+    isinf(x) && convert(T, sign(x)*Inf)
     x.c
 end
 
@@ -25,14 +35,14 @@ end
 @inline realpart(l::LogNumber) = l.c
 
 
-Base.promote_rule(::Type{LogNumber{T}}, ::Type{R}) where {T, R<:Real} = LogNumber{promote_type(T,R)}
+Base.promote_rule(::Type{LogNumber{T}}, ::Type{R}) where {T, R<:RealOrComplex} = LogNumber{promote_type(T,R)}
 Base.convert(::Type{LogNumber}, z::LogNumber) = z
-Base.convert(::Type{LogNumber}, z::Real) = LogNumber(0, z)
+Base.convert(::Type{LogNumber}, z::RealOrComplex) = LogNumber(0, z)
 
 ==(a::LogNumber, b::LogNumber) = logpart(a) == logpart(b) && realpart(a) == realpart(b)
 Base.isapprox(a::LogNumber, b::LogNumber; opts...) = ≈(logpart(a), logpart(b); opts...) && ≈(realpart(a), realpart(b); opts...)
-Base.isapprox(a::LogNumber, b::Real; opts...) = ≈(a(1),b; opts...)
-Base.isapprox(a::Real, b::LogNumber; opts...) = ≈(a,b(1); opts...)
+Base.isapprox(a::LogNumber, b::RealOrComplex; opts...) = ≈(a(1),b; opts...)
+Base.isapprox(a::RealOrComplex, b::LogNumber; opts...) = ≈(a,b(1); opts...)
 
 (l::LogNumber)(ε) = logpart(l)*log(ε) + realpart(l)
 
@@ -48,51 +58,59 @@ end
 for f in (:+, :-)
     @eval begin
         $f(a::LogNumber, b::LogNumber) = LogNumber($f(a.s, b.s), $f(a.c, b.c))
-        $f(l::LogNumber, b::Real) = LogNumber(l.s, $f(l.c, b))
-        $f(a::Real, l::LogNumber) = LogNumber($f(l.s), $f(a, l.c))
+        $f(l::LogNumber, b::RealOrComplex) = LogNumber(l.s, $f(l.c, b))
+        $f(a::RealOrComplex, l::LogNumber) = LogNumber($f(l.s), $f(a, l.c))
     end
 end
 
 -(l::LogNumber) = LogNumber(-l.s, -l.c)
 
-for Typ in (:Bool, :Real)
+for Typ in (:Bool, :Real, :Complex)
     @eval begin
         *(l::LogNumber, b::$Typ) = LogNumber(l.s*b, l.c*b)
         *(a::$Typ, l::LogNumber) = LogNumber(a*l.s, a*l.c)
     end
 end
 
+muladd(x::RealOrComplex, y::LogNumber, z::LogNumber) = x*y + z
+
 function /(a::LogNumber, b::LogNumber)
     if !isinf(b)
-        a / Real(b)
+        a / RealOrComplex(b)
     elseif !isinf(a)
-        Real(a) / b
+        RealOrComplex(a) / b
     else
         a.s / b.s
     end
 end
-/(l::LogNumber, b::Real) = LogNumber(l.s/b, l.c/b)
+/(l::LogNumber, b::RealOrComplex) = LogNumber(l.s/b, l.c/b)
 
-function /(a::Real, l::LogNumber)
+function /(a::RealOrComplex, l::LogNumber)
     isinf(l) && throw(DomainError(l))
-    a / Real(l)
+    a / RealOrComplex(l)
 end
 
 exp(l::LogNumber) = PowerNumber(exp(l.c), 0, l.s, l.s)
 expm1(l::LogNumber) = exp(l) - 1
 
-abs(l::LogNumber) = LogNumber(abs(l.s), abs(l.c))
+function abs(l::LogNumber)
+    isinf(l) && throw(DomainError(l))
+    abs(RealOrComplex(l))
+end
 isless(a::LogNumber, b::LogNumber) = a.s == b.s ? isless(a.c, b.c) : isless(b.s, a.s)
 function isless(a::LogNumber, b::Number)
     if isinf(a)
         signbit(a) && (!isinf(b) || !signbit(b))
     else
-        isless(Real(a), b)
+        isless(RealOrComplex(a), b)
     end
 end
 
 isinf(a::LogNumber) = !iszero(a.s)
-isreal(a::LogNumber) = iszero(a.s)
+isreal(a::LogNumber) = isreal(a.s) && isreal(a.c)
+
+sign(a::LogNumber) = isinf(a) ? -sign(a.s) : sign(a.c)
+signbit(a::LogNumber) = isinf(a) ? !signbit(a.s) : sign(a.c)
 
 
 Base.show(io::IO, x::LogNumber) = print(io, "($(logpart(x)))log ε + $(realpart(x))")
